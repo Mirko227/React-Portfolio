07b8b607a11ca88d2e84d569cc651fe1706bec0e
Mirko227 <mirkobellopede22@gmail.com>
2024-09-09 00:59:44 +0200

    aggiunto link alle immagine pagina progetti

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -8,35 +8,46 @@ import { Project } from "../locales/interfaces";
 
 function ProjectsPage() {
   const { t } = useTranslation();
-  
+
   // Specify the type for the projectsData array
-  const projectsData : Project[] = t("projects.data", { returnObjects: true });
-  
+  const projectsData: Project[] = t("projects.data", { returnObjects: true });
+
   return (
     <>
       <div id={projects.mainContainer}>
         <div className={projects.container}>
           <h2>{t("projects.title")}</h2>
-          {projectsData.map(({ id, name, imgPath, description, languages,link }) => (
-            <div className={projects.projectCard} key={id}>
-              <div className={projects.imgWrapper}>
-                <IKImage 
-                  urlEndpoint={imagekitConfig.urlEndpoint} 
-                  path={imgPath} 
-                  alt={name}
-                />
+          {projectsData.map(
+            ({ id, name, imgPath, description, languages, link }) => (
+              <div className={projects.projectCard} key={id}>
+                <div className={projects.imgWrapper}>
+                  <a href={link} target="_blank" rel="noopener noreferrer">
+                    <IKImage
+                      urlEndpoint={imagekitConfig.urlEndpoint}
+                      path={imgPath}
+                      alt={name}
+                    />
+                  </a>
+                </div>
+                <div className={projects.projectInfo}>
+                  <a
+                    className={`${projects.projectName} primary-hover`}
+                    href={link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {name}
+                  </a>
+                  <ul className={projects.devInfo}>
+                    {languages.map((element: string, index: number) => (
+                      <li key={`devInfoItem-${id}-${index}`}>{element}</li> // Use a combination of id and index to ensure a unique key
+                    ))}
+                  </ul>
+                  <p>{description}</p>
+                </div>
               </div>
-              <div className={projects.projectInfo}>
-                <a className={`${projects.projectName} primary-hover`} href={link} target="_blank" rel="noopener noreferrer">{name}</a>
-                <ul className={projects.devInfo}>
-                  {languages.map((element : string, index : number) => (
-                    <li key={`devInfoItem-${id}-${index}`}>{element}</li> // Use a combination of id and index to ensure a unique key
-                  ))}
-                </ul>
-                <p>{description}</p>
-              </div>
-            </div>
-          ))}
+            )
+          )}
         </div>
         <Footer />
       </div>
